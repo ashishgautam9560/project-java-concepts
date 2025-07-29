3c0dfdc7197c3c3ee7b3657a961cda0ada2e8d8f
Ashish Gautam <68273485+ashishgautam9560@users.noreply.github.com>
2025-07-29 21:10:59 +0530

    Add files via upload

---
+++ src/main/java/com/project/java/webapp/concepts/ProjectJavaConceptsApplication.java
@@ -0,0 +1,13 @@
+package com.project.java.webapp.concepts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectJavaConceptsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectJavaConceptsApplication.class, args);
+	}
+
+}
