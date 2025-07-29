8d0609a0c8e1167920f2434553f1ac2c93a23803
Ashish Gautam <68273485+ashishgautam9560@users.noreply.github.com>
2025-07-29 21:09:53 +0530

    Add files via upload

---
+++ project-java-concepts/src/test/java/com/project/java/webapp/concepts/ProjectJavaConceptsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.java.webapp.concepts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectJavaConceptsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
